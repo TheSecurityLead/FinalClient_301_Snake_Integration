806c5675b01525b5922f70db75eebf996b57d583
David Robertson <127712361+TheSecurityLead@users.noreply.github.com>
2023-12-21 00:38:53 +0300

    Update SnakeGame.jsx

--- FinalIntegration/SnakeGame.jsx
+++ FinalIntegration/SnakeGame.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GameBoard from './GameBoard';
 import ScoreBoard from './ScoreBoard';
 import ControlPanel from './ControlPanel';
 
 function SnakeGame() {
+    // State variables
+    const [currentScore, setCurrentScore] = useState(0);
+    const [highScore, setHighScore] = useState(0);
+    const [snake, setSnake] = useState([{ x: 10, y: 10 }]); // Example initial state
+    const [food, setFood] = useState({ x: 20, y: 20 }); // Example initial state
+    const [isGameActive, setIsGameActive] = useState(false);
+
+    // Game control functions
+    const startGame = () => {
+        setIsGameActive(true);
+        // Add logic to start the game
+    };
+
+    const pauseGame = () => {
+        setIsGameActive(false);
+        // Add logic to pause the game
+    };
+
+    const resetGame = () => {
+        setIsGameActive(false);
+        setCurrentScore(0);
+        // Add logic to reset the game
+    };
+
     return (
-        <div className="snake-game-container">
-            <ScoreBoard />
-            <GameBoard />
-            <ControlPanel />
+        <div>
+            <ScoreBoard currentScore={currentScore} highScore={highScore} />
+            <GameBoard snake={snake} food={food} />
+            <ControlPanel onStart={startGame} onPause={pauseGame} onReset={resetGame} />
         </div>
     );
 }
