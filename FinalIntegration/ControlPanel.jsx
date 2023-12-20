effafa85984ea3be3300e01e437982951d4dcdc1
David Robertson <127712361+TheSecurityLead@users.noreply.github.com>
2023-12-21 02:26:49 +0300

    Update ControlPanel.jsx

--- FinalIntegration/ControlPanel.jsx
+++ FinalIntegration/ControlPanel.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
-function ScoreBoard({ currentScore, highScore }) {
+function GameControlsAndScore({ 
+    onStart, onPause, onReset, 
+    currentScore, highScore 
+}) {
     return (
-        <div className="score-board">
-            <div>Current Score: {currentScore}</div>
-            <div>High Score: {highScore}</div>
+        <div>
+            {/* Score display */}
+            <div className="score-board">
+                <div>Current Score: {currentScore}</div>
+                <div>High Score: {highScore}</div>
+            </div>
+
+            {/* Control panel buttons */}
+            <div className="control-panel">
+                <button onClick={onStart}>Start</button>
+                <button onClick={onPause}>Pause</button>
+                <button onClick={onReset}>Reset</button>
+            </div>
+
+            
         </div>
     );
 }
 
-export default ScoreBoard;
+export default GameControlsAndScore;
