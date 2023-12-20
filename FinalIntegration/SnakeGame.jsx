e935da1d86936d5e8701234627340226bd634eb9
David Robertson <127712361+TheSecurityLead@users.noreply.github.com>
2023-12-21 02:16:38 +0300

    Update SnakeGame.jsx

--- FinalIntegration/SnakeGame.jsx
+++ FinalIntegration/SnakeGame.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import GameBoard from './GameBoard';
 import ScoreBoard from './ScoreBoard';
 import ControlPanel from './ControlPanel';
@@ -9,110 +9,92 @@ function SnakeGame() {
     const [soundEnabled, setSoundEnabled] = useState(true);
     const [currentScore, setCurrentScore] = useState(0);
     const [highScore, setHighScore] = useState(0);
-    const [snake, setSnake] = useState([{ x: 10, y: 10 }]); // Example initial state
-    const [food, setFood] = useState({ x: 20, y: 20 }); // Example initial state
+    const [snake, setSnake] = useState([{ x: 10, y: 10 }]);
+    const [food, setFood] = useState({ x: 20, y: 20 });
     const [isGameActive, setIsGameActive] = useState(false);
-  const getRandomFoodPosition = () => {
+    const [gameOver, setGameOver] = useState(false);
+    const [hasEatenFood, setHasEatenFood] = useState(false);
+    const [gameStarted, setGameStarted] = useState(false);
+    const [hasSnakeTurned, setHasSnakeTurned] = useState(false);
+
+    const canvasSize = 600; // Example size, adjust as needed
+    const boxSize = 20; // Example size, adjust as needed
+    const direction = useRef('right'); // Example initial direction
+
+    const gameLoopIntervalRef = useRef(null);
+
+    const getRandomFoodPosition = () => {
         const newFood = {
-            x: Math.floor(Math.random() * (canvasSize / boxSize)),
-            y: Math.floor(Math.random() * (canvasSize / boxSize)),
+            x: Math.floor(Math.random() * (canvasSize / boxSize)) * boxSize,
+            y: Math.floor(Math.random() * (canvasSize / boxSize)) * boxSize,
         };
         return newFood;
     };
-    // Game control functions
-const startGame = () => {
-    setIsGameActive(true);
 
-    // Initialize snake and food positions
-    const initialSnake = [{ x: 10, y: 10 }]; // You can change the initial position
-    const initialFood = getRandomFoodPosition(); // Implement this function
-
-    setSnake(initialSnake);
-    setFood(initialFood);
-    setCurrentScore(0); // Initialize the score
-
-    // Set up game loop interval
-    const gameLoopInterval = setInterval(() => {
-        // Update snake's position based on the current direction
-        const newSnakeHead = { ...snake[0] };
-        switch (direction) {
-            case 'up':
-                newSnakeHead.y -= 1;
-                break;
-            case 'down':
-                newSnakeHead.y += 1;
-                break;
-            case 'left':
-                newSnakeHead.x -= 1;
-                break;
-            case 'right':
-                newSnakeHead.x += 1;
-                break;
-            default:
-                break;
+    const handleGameOver = () => {
+        setIsGameActive(false);
+        setGameOver(true);
+        if (currentScore > highScore) {
+            setHighScore(currentScore);
         }
+    };
 
-        // Check for collisions with food
-        if (newSnakeHead.x === food.x && newSnakeHead.y === food.y) {
-            // Snake eats the food
-            const newSnake = [newSnakeHead, ...snake];
-            const newFood = getRandomFoodPosition();
-            setCurrentScore(currentScore + 1); // Increase the score
-            setSnake(newSnake);
-            setFood(newFood);
-        } else {
-            // Move the snake by adding the new head and removing the tail
-            const newSnake = [newSnakeHead, ...snake.slice(0, -1)];
-            setSnake(newSnake);
-        }
+    const startGame = () => {
+        setGameStarted(true);
+        setIsGameActive(true);
+        setGameOver(false);
+        setHasEatenFood(false);
+        setHasSnakeTurned(false);
 
-        // Check for collisions with walls
-        if (
-            newSnakeHead.x < 0 ||
-            newSnakeHead.x >= canvasSize / boxSize ||
-            newSnakeHead.y < 0 ||
-            newSnakeHead.y >= canvasSize / boxSize
-        ) {
-            handleGameOver();
-        }
+        const initialSnake = [{ x: 10, y: 10 }];
+        const initialFood = getRandomFoodPosition();
 
-        // Check for collisions with itself
-        if (newSnake.some((segment, index) => index !== 0 && segment.x === newSnakeHead.x && segment.y === newSnakeHead.y)) {
-            handleGameOver();
-        }
-    }, 100); // Adjust the interval as needed for your game speed
+        setSnake(initialSnake);
+        setFood(initialFood);
+        setCurrentScore(0);
 
-};
+        gameLoopIntervalRef.current = setInterval(() => {
+            // Game loop logic here
+            // ...
+        }, 100);
+    };
 
     const pauseGame = () => {
         setIsGameActive(false);
-        // Add logic to pause the game
+        clearInterval(gameLoopIntervalRef.current);
     };
 
     const resetGame = () => {
         setIsGameActive(false);
         setCurrentScore(0);
-        // Add logic to reset the game
+        clearInterval(gameLoopIntervalRef.current);
     };
+
+    // Cleanup interval on component unmount
+    useEffect(() => {
+        return () => {
+            clearInterval(gameLoopIntervalRef.current);
+        };
+    }, []);
 
     return (
         <div>
-      {/* Include the SoundManager component */}
-      <SoundManager soundEnabled={soundEnabled} />
-      <ScoreBoard currentScore={currentScore} highScore={highScore} />
-      <GameBoard snake={snake} food={food} />
-      <ControlPanel
-        onStart={startGame}
-        onPause={pauseGame}
-        onReset={resetGame}
-        onToggleSound={() => setSoundEnabled(!soundEnabled)}
-        soundEnabled={soundEnabled}
-      />
-    </div>
-        <div>
+            <SoundManager
+                soundEnabled={soundEnabled}
+                isGameOver={gameOver}
+                hasEatenFood={hasEatenFood}
+                isGameStarted={gameStarted}
+                hasSnakeTurned={hasSnakeTurned}
+            />
             <ScoreBoard currentScore={currentScore} highScore={highScore} />
             <GameBoard snake={snake} food={food} />
-            <ControlPanel onStart={startGame} onPause={pauseGame} onReset={resetGame} />
+            <ControlPanel
+                onStart={startGame}
+                onPause={pauseGame}
+                onReset={resetGame}
+                onToggleSound={() => setSoundEnabled(!soundEnabled)}
+                soundEnabled={soundEnabled}
+            />
         </div>
     );
 }
